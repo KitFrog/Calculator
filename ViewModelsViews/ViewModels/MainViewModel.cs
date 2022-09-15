9a8c34222c30bb1c7afbacacaf32541a88b1e65d
popovns <popovns@400D-1>
2022-09-15 14:32:51 +0700

    Исправение ошибок, добавление кнопки

--- ViewModelsViews/ViewModels/MainViewModel.cs
+++ ViewModelsViews/ViewModels/MainViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Models;
 
 namespace ViewModelsViews.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        private string display = "Some text";
-        public string Display{ get=>display; set {
+        private Calulations model = new Calulations();
+        private string display = "";
+        public string Display
+        {
+            get => display;
+            set
+            {
                 display = value;
                 OnPropertyChanged("Display");
             }
