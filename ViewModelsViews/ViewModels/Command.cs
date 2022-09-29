182884b17a2c59e26677181c6a830ce12c3432a6
popovns <popovns@400D-1>
2022-09-29 14:34:35 +0700

    Много чего было добавленно

---
+++ ViewModelsViews/ViewModels/Command.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModelsViews.ViewModels
+{
+    public class Command : ICommand
+    {
+
+        public event EventHandler CanExecuteChanged;
+
+        private Action executeMethod;
+        private Func<bool> canExecute;
+
+        public Command(Action executeMethod, Func<bool> canExecute = null)
+        {
+            this.executeMethod = executeMethod;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object _)
+        {
+            return canExecute == null ? true : canExecute();
+        }
+
+        public void Execute(object _)
+        {
+            if (CanExecute(null))
+            {
+                executeMethod.Invoke();
+            }
+        }
+
+        public void RaiseCanExecuteChanged() =>
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public class Command<T> : ICommand
+    {
+
+        public event EventHandler CanExecuteChanged;
+
+        private Action<T> executeMethod;
+        private Func<T, bool> canExecute;
+
+        public Command(Action<T> executeMethod, Func<T, bool> canExecute = null)
+        {
+            this.executeMethod = executeMethod;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object param)
+        {
+            return canExecute == null ? true : canExecute((T)param);
+        }
+
+        public void Execute(object param)
+        {
+            if (CanExecute(null))
+            {
+                executeMethod.Invoke((T)param);
+            }
+        }
+
+        public void RaiseCanExecuteChanged() =>
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
--- ViewModelsViews/ViewModels/MainViewModel.cs
+++ ViewModelsViews/ViewModels/MainViewModel.cs
@@ -9,18 +9,131 @@ namespace ViewModelsViews.ViewModels
     public class MainViewModel : ViewModelBase
     {
         public string Comma => CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
-        private Calulations model = new Calulations();
-        private string display = "543/10";
+        private Calculations model = new Calculations();
+        private string displayIn = "0";
+        private string lastOperation = "";
 
-        public string Display
+        public string DisplayIn
         {
-            get => display;
+            get => displayIn;
             set
             {
-                display = value;
-                OnPropertyChanged("Display");
+                displayIn = value;
+                CommaPress.RaiseCanExecuteChanged();
+                OnPropertyChanged("DisplayIn");
             }
         }
 
+        private string displayOut = "";
+
+        public string DisplayOut
+        {
+            get => displayOut;
+            set
+            {
+                displayOut = value;
+                OnPropertyChanged("displayOut");
+            }
+        }
+
+        public Command<string> DigitPress { get; }
+        public Command<string> CommandPress { get; }
+        public Command SymbolChange { get; }
+        public Command CommaPress { get; }
+        public Command Delete { get; }
+        public Command Clear { get; }
+        public Command Result { get; }
+
+        public MainViewModel()
+        {
+            DigitPress = new Command<string>(digitPress);
+            CommandPress = new Command<string>(commandPress);
+            SymbolChange = new Command(symbolChange);
+            CommaPress = new Command(commaPress, () => !DisplayIn.Contains(Comma));
+            Delete = new Command(delete);
+            Clear = new Command(clear);
+            Result = new Command(result);
+        }
+
+        private void commandPress(string param)
+        {
+            if (lastOperation == "")
+            {
+                DisplayOut = DisplayIn + param;
+                model.FirstOperand = DisplayIn;
+                model.Operation = param;
+                DisplayIn = "0";
+                lastOperation = param;
+            }
+        }
+        private void result()
+        {
+            try
+            {
+                model.Calulate();
+                DisplayOut += $" = {model.Result}";
+            }
+            catch
+            {
+                if(string.IsNullOrEmpty(model.SecondOperand))
+                {
+                    DisplayOut = "";
+                    DisplayIn = model.FirstOperand;
+                }
+            }
+        }
+
+        private void digitPress(string param)
+        {
+            if (DisplayIn == "0")
+            {
+                DisplayIn = param;
+            }
+            else if (DisplayIn == "-0")
+            {
+                DisplayIn = $"-{param}";
+            }
+            else
+            {
+                DisplayIn += param;
+            }
+            if (string.IsNullOrEmpty(lastOperation))
+            {
+                model.FirstOperand = DisplayIn;
+            }
+            else
+            {
+                model.SecondOperand = DisplayIn;
+            }
+        }
+
+        private void commaPress()
+        {
+            DisplayIn += Comma;
+        }
+        private void delete()
+        {
+            if (DisplayIn == "0" || DisplayIn == "-0") return;
+
+            DisplayIn = DisplayIn.Remove(DisplayIn.Length-1);
+        }
+        private void clear()
+        {
+            DisplayIn = "0";
+            DisplayOut = "";
+            model = new Calculations();
+        }
+
+        private void symbolChange()
+        {
+            if (DisplayIn.StartsWith('-'))
+            {
+                DisplayIn = DisplayIn.Remove(0, 1);
+            }
+            else
+            {
+                DisplayIn = DisplayIn.Insert(0, "-");
+            }
+        }
     }
 }
