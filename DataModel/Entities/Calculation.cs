db0b853f59e8492be51188eb8af0c2c1b6ab1c17
popovns <popovns@400D-1>
2022-11-16 14:34:43 +0700

    Начали реализацию базы данных

---
+++ DataModel/Entities/Calculation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Entities
+{
+    public class Calculation
+    {
+        public Guid Id { get; set; }
+        public string Result { get; set; } = null!;
+        public string FirstOperand { get; set; } = null!;
+        public string? SecondOperand { get; set; }
+        public string Operation { get; set; } = null!;
+
+        public static bool ContextEqual(Calculation left, Calculation right)
+        {
+            if (left == null || right == null) return false;
+            return left.Result == right.Result && left.Operation == right.Operation && left.FirstOperand == right.FirstOperand;
+        }
+    }
+}
---
+++ DataModel/Entities/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Entities
+{
+    public class History
+    {
+        public Guid Id { get; set; }
+        public DateTime ActualTime { get; set; } = DateTime.Now;
+        public bool IsGuest { get; set; } = true;
+        public Guid CalculationId { get; set; }
+        public Calculation Calculation { get; set; } = null!;
+    }
+}
